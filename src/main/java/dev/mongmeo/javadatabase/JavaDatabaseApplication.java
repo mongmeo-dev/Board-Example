c6bcff9be71a7e0aeca7aebf8880826ffa51d61c
Hongdae Kim <mongmeo.dev@gmail.com>
2022-11-11 08:42:32 +0900

    initial commit

---
+++ src/main/java/dev/mongmeo/javadatabase/JavaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package dev.mongmeo.javadatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaDatabaseApplication.class, args);
+  }
+
+}
